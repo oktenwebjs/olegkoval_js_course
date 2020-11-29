520d74b6c0d3000b485ecc76920ba35cb7f088d4
Oleg Koval <contact@olegkoval.com>
2020-11-29 22:13:13 +0100

    Update homework #14

--- homework_14/script.js
+++ homework_14/script.js
@@ -2,6 +2,7 @@ async function printText() {
     let textElm = document.querySelector('#text_to_print');
     let printElm = document.querySelector('#printer');
     if (textElm && printElm) {
+        printElm.innerHTML = '';
         let textArr = textElm.value.split('');
         for (let charVal of textArr) {
             await new Promise(resolve => {
