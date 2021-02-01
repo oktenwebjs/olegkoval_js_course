13d2cbe545d565b326222f59408f54a640e4241d
freefireman <beluh.c.s@gmail.com>
2021-02-01 15:36:07 +0200

    little fix)) delete excess component

--- React/homework_redux_3/src/App.js
+++ React/homework_redux_3/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import {Header} from "./components/header";
-import {Actions} from "./components/actions";
+import {ActionButtons} from "./components/action-buttons";
+import {ActionForm} from "./components/action-form";
 
 
 function App() {
     return (
         <div className="App">
             <Header/>
-            <Actions />
+            <ActionButtons/>
+            <ActionForm/>
         </div>
     );
 }
